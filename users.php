54f1e22cd6fdbe5177949b9e0d67540b1bec5785
xhoshua <mucaxhoi3@gmail.com>
2021-11-04 16:55:37 +0100

    change

--- users.php
+++ users.php
@@ -7,6 +7,58 @@
 <body>
 <?php
 include ('connection.php');
+?>
+<?php
+if (isset($_POST['Submit_User_Modify'])){
+    $errorCount = 0;
+    $firstName = $_POST['first_name'];
+    if (empty($firstName)) {
+        $errorCount++;
+        echo 'Emri duhet te vendoset <br>';
+    } else if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
+        echo 'Emri duhet vetem me shkronja dhe hapsira <br>';
+        $errorCount++;
+    }
+    $lastName = $_POST['last_name'];
+    if (empty($lastName)) {
+        $errorCount++;
+        echo 'Mbiemri duhet te vendoset <br>';
+    } else if (!preg_match("/^[a-zA-Z-' ]*$/",$lastName)) {
+        echo 'Mbiemri duhet vetem me shkronja dhe hapsira <br>';
+        $errorCount++;
+    }
+    $email = $_POST['email'];
+    if (empty($email)) {
+        $errorCount++;
+        echo 'Email eshte i detyrueshem';
+    } else if(!filter_input(INPUT_POST,"email", FILTER_VALIDATE_EMAIL)){
+        echo "Email isn't valid <br>";
+        $errorCount++;
+    }
+    if($errorCount===0){
+        $query="UPDATE users SET first_name=:first_name,last_name=:last_name,email=:email WHERE id=:id limit 1";
+        $stmt= $db->prepare($query);
+        if ($stmt->execute([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $email,
+            'id' => $_POST['id'],
+        ])){
+            echo "Useri u modifikua me sukses <br>";
+        } else {
+            echo "Provoje perseri";
+        }
+    }
+}
+?>
+<?php
+if (isset($_GET['action']) && $_GET['action']== 'delete_user'){
+    $query="DELETE FROM users WHERE id=:id";
+    $stmt= $db->prepare($query);
+    $stmt->execute(['id' => $_GET['user_id']]);
+    ?>
+<?php } ?>
+<?php
 $query= "select id,first_name,last_name,email,created_at,updated_at from users";
 $stmt= $db->prepare($query);
 $stmt->execute();
@@ -39,29 +91,36 @@ $users = $stmt->fetchAll();
             <td><?=$user['updated_at']?></td>
             <td>
                 <a href="users.php?action=modify_user&user_id=<?=$user['id']?>">Modify</a>
+                <a href="users.php?action=delete_user&user_id=<?=$user['id']?>"> Delete </a>
             </td>
-        </tr>
+            </tr>
     <?php } ?>
     </tbody>
 
 </table>
 <?php
+
+
 if(isset( $_GET['action']) && $_GET['action'] == 'modify_user'){
-    $query="select first_name,last_name,email from users where id=:id limit 1";
+    $query="select id, first_name,last_name,email from users where id=:id limit 1";
     $stmt= $db->prepare($query);
     $stmt->execute(['id' => $_GET['user_id']]);
- $user= $stmt->fetch();
+    $user= $stmt->fetch();
     ?>
-<form method="get">
+<form method="post" action="users.php">
     <label>Modify <br></label>
-<label>First Name</label>
+    <input type="hidden" name="id" value="<?=$user['id']?>"/><br>
+    <label>First Name</label>
     <input type="text" name="first_name" value="<?=$user['first_name']?>"/><br>
     <label>Last Name</label>
     <input type="text" name="last_name" value="<?=$user['last_name']?>"/><br>
     <label>Email</label>
     <input type="text" name="email" value="<?=$user['email']?>"/><br>
-    <input type="submit" name="Submit_Changes"/><br>
+    <input type="submit" name="Submit_User_Modify"/><br>
 </form>
-<?php }?>
+
+<?php } ?>
+
+
 </body>
 </html>
